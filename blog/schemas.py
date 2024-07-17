db0a33a21a6a06f74f71afc3967c1e98666430e4
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2024-07-17 19:48:57 +0300

    Pydantic schemas

---
+++ blog/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+    
+class Blog(BlogBase):
+    class Config():
+        from_attributes = True
+        
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+    
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: list[Blog] = []
+    class Config():
+        from_attributes = True
+        
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        from_attributes = True

8f3dd9481b8dbe449db6c650ab2459f8cbbe1c49
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2024-07-17 21:12:13 +0300

    Login and Token schema

--- blog/schemas.py
+++ blog/schemas.py
@@ -13,6 +13,17 @@ class User(BaseModel):
     email: str
     password: str
     
+class Login(BaseModel):
+    username: str
+    password: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: str | None = None
+    
 class ShowUser(BaseModel):
     name: str
     email: str

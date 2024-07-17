b483f032e4c2141b833670e711c8b6096d192e31
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2024-07-17 13:25:48 +0300

    Request Body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -17,8 +18,16 @@ def show(id: int):
     # Fetch blog with id = id
     return {'data': id}
 
-
 @app.get('/blog/{id}/comments')
 def comments(id: int, limit = 10):
     # Fetch (limit) comments of blog with id = id
     return {'data': {'1', '2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title as {blog.title}'}
